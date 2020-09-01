7a15fcb80e9c974e811c537534e9629a6e7acb32
zhangfan <pku_zf@pku.edu.cn>
2020-09-01 15:19:09 +0800

    use 1 time

--- temp.py
+++ temp.py
@@ -15,8 +15,8 @@ mydb = mysql.connector.connect(
 )
  
 # print(mydb)
-for m in range(1,73): # m为计数器，有多少行数据后面的数+1
-    p = "SELECT C3 FROM test12 WHERE C1 = "+str(m) #为拼接sql语句，取出关键词数据sql语句
+for m in range(1,2021): # m为计数器，有多少行数据后面的数+1
+    p = "SELECT C3 FROM data WHERE C1 = "+str(m) #为拼接sql语句，取出关键词数据sql语句
     print(p)
     mycursor = mydb.cursor() # 初始化数据库游标
     mycursor.execute(p)      #执行sql语句，完成关键词数据提取
@@ -26,7 +26,7 @@ for m in range(1,73): # m为计数器，有多少行数据后面的数+1
     k = "%"+h+"%"   #把关键词用%隔开
     print(k)
     x = "\""+k+"\""
-    g = "SELECT count(*) FROM test12 where C2 like "+x #求得和C3相似的C2总数
+    g = "SELECT count(*) FROM data where C2 like "+x #求得和C3相似的C2总数
     print(g)
  #  mycursor = mydb.cursor()
     mycursor.execute(g)
@@ -34,6 +34,6 @@ for m in range(1,73): # m为计数器，有多少行数据后面的数+1
     print(d)
     b = d[0]
     print(b)
-    a = "UPDATE test12 SET C4 = "+str(b)+" WHERE C1 = "+str(m)
+    a = "UPDATE data SET C4 = "+str(b)+" WHERE C1 = "+str(m)
     print(a)
     mycursor.execute(a) #把重复量写回数据库，不是1的即为有重复
